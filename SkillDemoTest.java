6564d76ff721c12d0d6bb66bb38fcc27c3807a68
Nowcarpediem <dchu@ucsd.edu>
2022-02-03 14:10:32 -0800

    libs + code that fails

---
+++ SkillDemoTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class SkillDemoTest {
+    @Test
+    public void multiply() {
+        assertEquals(6, SkillDemo.multiply(2, 3));
+    }
+}
